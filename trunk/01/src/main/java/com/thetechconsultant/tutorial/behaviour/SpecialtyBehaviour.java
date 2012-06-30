9a13b9642e17a92c09466047c5aedfeb3ef7f9d7
paco.antunez@gmail.com <paco.antunez@gmail.com@daafd1c7-0409-8b8f-f3e4-b5e923b61d6b>
2012-06-30 18:17:05 +0000

    Creating a new content model.

---
+++ trunk/01/src/main/java/com/thetechconsultant/tutorial/behaviour/SpecialtyBehaviour.java
@@ -0,0 +1,7 @@
+package com.thetechconsultant.tutorial.behaviour;
+
+import org.alfresco.repo.node.NodeServicePolicies.OnCreateNodePolicy;
+
+public interface SpecialtyBehaviour extends OnCreateNodePolicy {
+
+}
