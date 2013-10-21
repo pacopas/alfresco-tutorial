9f94f46f4089fcbe2dc9afa6a4082dae9a360125
paco.antunez <paco.antunez@daafd1c7-0409-8b8f-f3e4-b5e923b61d6b>
2013-10-21 22:31:32 +0000

    Second iteration

--- trunk/insert-records/src/main/java/ie/paco/ir/impl/CsvRecordReader.java
+++ trunk/insert-records/src/main/java/ie/paco/ir/impl/CsvRecordReader.java
@@ -2,18 +2,36 @@ package ie.paco.ir.impl;
 
 import ie.paco.ir.RecordReader;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class CsvRecordReader implements RecordReader {
+
+	private static final Log LOG = LogFactory.getLog(CsvRecordReader.class);
 
 	private BufferedReader reader;
 
 	@Override
-	public boolean openResource(String[] config) {
-		return false;
+	public boolean openResource(String[] config) throws IOException {
+		boolean opened = false;
+		if (config == null || config.length == 0) {
+			LOG.error("Resource hasn't been specified");
+		} else {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(config[0])));
+			opened = true;
+		}
+		return opened;
 	}
 	
 	@Override
-	public String[] nextRecord() {
-		return null;
+	public String[] nextRecord() throws IOException {
+		return reader.readLine().split(",");
+	}
+	
+	public BufferedReader getReader() {
+		return reader;
 	}
 }
