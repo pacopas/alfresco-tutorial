19ab0dfb5ff539e8fe5f3fd5971d5b5b995de5ec
paco.antunez <paco.antunez@daafd1c7-0409-8b8f-f3e4-b5e923b61d6b>
2013-10-21 22:31:32 +0000

    Second iteration

--- insert-records/src/main/java/ie/paco/ir/RecordReader.java
+++ insert-records/src/main/java/ie/paco/ir/RecordReader.java
@@ -1,8 +1,10 @@
 package ie.paco.ir;
 
+import java.io.IOException;
+
 public interface RecordReader {
 
-	public boolean openResource(String[] config);
-	public String[] nextRecord();
+	public boolean openResource(String[] config) throws IOException;
+	public String[] nextRecord() throws IOException;
 	
 }
--- insert-records/src/test/java/ie/paco/ir/impl/CsvRecordReaderTest.java
+++ insert-records/src/test/java/ie/paco/ir/impl/CsvRecordReaderTest.java
@@ -7,19 +7,55 @@ import java.io.FileNotFoundException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
 
 public class CsvRecordReaderTest {
 
 	private CsvRecordReader recordReader;
+	private final String filePath = "src/test/test.csv";
+	private File testFile;
 	
 	@Before
 	public void setUp() {
 		recordReader = new CsvRecordReader();
+		testFile = new File(filePath);
+		testFile.setReadable(true, false);
 	}
 	
 	@Test(expected = FileNotFoundException.class)
-	public void testOpenResource_NOT_FOUND() {
+	public void testOpenResource_NOT_FOUND() throws IOException {
 		boolean opened = recordReader.openResource(new String[]{"not_found.csv"});
+	}
+	
+	@Test
+	public void testOpenResource_NULL_CONFIG() throws IOException {
+		boolean opened = recordReader.openResource(null);
+		assertFalse(opened);
+		assertNull(recordReader.getReader());
+	}
+	
+	@Test
+	public void testOpenResource_EMPTY_CONFIG() throws IOException {
+		boolean opened = recordReader.openResource(new String[0]);
+		assertFalse(opened);
+		assertNull(recordReader.getReader());
+	}
+	
+	@Test
+	public void testOpenResource() throws IOException {
+		boolean opened = recordReader.openResource(new String[]{filePath});
+		assertTrue(opened);
+		assertNotNull("reader shouldn't be null", recordReader.getReader());
+	}
+	
+	@Test(expected = IOException.class)
+	public void testNextRecord() throws IOException {
+		try {
+			recordReader.openResource(new String[]{filePath});
+		} catch (IOException ioe) {}
+		testFile.setReadable(false, false);
+		recordReader.nextRecord();
 	}
 	
 }
